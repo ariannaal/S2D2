53b2423d3ab02b6cb756799584fff36f82d0e405
ariannaal <ariannaloreti25@gmail.com>
2024-09-10 18:07:24 +0200

    adds authors

---
+++ S2D2/src/main/java/com/example/S2D2/controllers/AuthorController.java
@@ -0,0 +1,52 @@
+package com.example.S2D2.controllers;
+
+
+import com.example.S2D2.entities.Autore;
+import com.example.S2D2.services.AuthorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/authors")
+public class AuthorController {
+
+    @Autowired
+    private AuthorService authorService;
+
+    // http://localhost:3001/authors
+    @GetMapping
+    public List<Autore> getAllAuthors() {
+        return authorService.getAllAuthors();
+    }
+
+    // http://localhost:3001/authors / {id}
+    @GetMapping("/{id}")
+    private Autore getSingleAuthor(@PathVariable int id){
+        return authorService.findAuthorById(id);
+    }
+
+    // 2. POST http://localhost:3001/authors (+ post)
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    private Autore createAuthor(@RequestBody Autore autore){
+        return authorService.saveAuthor(autore);
+    }
+
+
+    @PutMapping("/{id}")
+    private Autore updateAuthor(@PathVariable int id, @RequestBody Autore autore){
+        return authorService.findAuthorAndUpdate(id, autore);
+    }
+
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    private void deleteAuthor(@PathVariable int id){
+        authorService.deleteAuthor(id);
+    }
+
+
+}
---
+++ S2D2/src/main/java/com/example/S2D2/services/AuthorService.java
@@ -0,0 +1,57 @@
+package com.example.S2D2.services;
+
+import com.example.S2D2.entities.Autore;
+import com.example.S2D2.entities.BlogPost;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class AuthorService {
+
+    private List<Autore> AuthorList = new ArrayList<>();
+
+    // ritorna tutti gli autori
+    public List<Autore> getAllAuthors() {
+        return this.AuthorList;
+    }
+
+    // salva gli autori
+    public Autore saveAuthor(Autore autore) {
+        this.AuthorList.add(autore);
+        return autore;
+    }
+
+    // ritorna un autore con un certo id
+    public Autore findAuthorById(int id) {
+        for (Autore autore : AuthorList) {
+            if (autore.getId() == id) {
+                return autore;
+            }
+        }
+        return null;
+    }
+
+    // modifica lo specifico autore
+    public Autore findAuthorAndUpdate(int id, Autore nuovoAutore) {
+        for (Autore autore : AuthorList) {
+            if (autore.getId() == id) {
+
+                autore.setNome(nuovoAutore.getNome());
+                autore.setCognome(nuovoAutore.getCognome());
+
+                return autore;
+            }
+        }
+        return null;
+    }
+
+    // elimina l'autore soecifico
+    public void deleteAuthor(int id) {
+        for (Autore autore : AuthorList) {
+            if (autore.getId() == id) {
+                this.AuthorList.remove(autore);
+            }
+        }
+    }
+}
